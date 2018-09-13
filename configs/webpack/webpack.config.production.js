e93d46071bcc7407bf9aac39058f2a03ffc74267
Blake Dietz <blakedietz@gmail.com>
2018-09-12 23:43:39 -0600

    build(config.yml): Add configurable asset path

--- configs/webpack/webpack.config.production.js
+++ configs/webpack/webpack.config.production.js
@@ -1,6 +1,20 @@
 const Merge = require("webpack-merge");
 const CommonConfig = require("./webpack.common.js");
+const webpack = require("webpack");
+
+// Try the environment variable, otherwise use root
+const ASSET_PATH = process.env.ASSET_PATH || '/';
 
 module.exports = Merge(CommonConfig, {
-  mode: "production"
+  mode: "production",
+  output: {
+    publicPath: ASSET_PATH
+  },
+
+  plugins: [
+    // This makes it possible for us to safely use env vars on our code
+    new webpack.DefinePlugin({
+      'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH)
+    })
+  ]
 });
